9e8f9cfe33e74474c3fc3c51c38118657bddf2a5
Mackenzie Schutz <mshoots64@gmail.com>
2020-04-09 10:39:13 -0500

    dynamically created planner display using jquery

--- script.js
+++ script.js
@@ -1,26 +1,54 @@
 $(document).ready(function() {
 
+    // const moment = require("moment");
+    // console.log(moment())
+
+
+// creating grid for planner
+for (let hour = 9; hour <= 17; hour++) {
+    let index = hour - 9;
+    $("#container").append(
+        $('<div>')
+            .addClass("row", "time-block")
+            .attr("id", "time-block")
+    );
+
+    $("#time-block").append(
+        $('<text>')
+            .addClass("hour")
+            .attr("id", "hour")
+            .text('hour-index', hour));
+
+    $("#time-block").append(
+        $('<textarea>'));
+
+    $("#time-block").append(
+        $('<button>')
+            .addClass("saveBtn")
+            .attr("id", "saveBtn")
+            .text("save"));
+    
+}
+    
+
+
+
+
 //create divs for timeslots 9-5 (9) rows with three columns. event space for user to type in. save button
 // Current hour is red 
 // When user types in plans and presses save button
     // Checks for input and if not input error message, if else append text to div
 
 
-var hours = ["9", "10", "11", "12", "13", "14", "15", "16", "17"]
-for (var i = 0; i < hours.length; i ++) {
-    var rowDiv = $("<div>")
-
-    rowDiv.addClass("row");
-
-    rowDiv.text(hours[i]);
-
-    $("#row").append(rowDiv);
-}
-
-
 //variable and function for current time
-var date = new Date();
-var time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-console.log(time);
+// var date = new Date();
+// var time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+// console.log(time);
+
+
+
+
+    // if time is 9:00-9:59.59 current background red .present
+    // if time is greater than time value- current background is green .future
 
 })
