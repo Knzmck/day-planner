6fb5243b3e6129548f5febd97fce0dd27224e0c1
Mackenzie Schutz <mshoots64@gmail.com>
2020-04-12 13:35:21 -0500

    creating local storage

--- script.js
+++ script.js
@@ -1,45 +1,50 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    // const moment = require("moment");
-    // console.log(moment())
+  var currentHour = moment().hours();
+  console.log(currentHour);
+  let timeBlock = ("#time-block")
 
+  $(timeBlock).each(function () {
+    let storedPlans = localStorage.getItem("plans");
+    if (storedPlans !== null) {
+      planTextArr = task;
+    } else {
+      // this should only occur on first time the app is loaded in the browser
+      // helpfully remind user that lunch is important
+      planTextArr = new Array(9);
+      planTextArr[4] = "Picnic lunch outside";
+    }
+    })
 
-// creating grid for planner
-for (let hour = 9; hour <= 17; hour++) {
+  // creating grid for planner
+  for (let hour = 9; hour <= 17; hour++) {
     let index = hour - 9;
     $("#container").append(
-        $('<div>')
-            .addClass("row", "time-block")
-            .attr("id", "time-block")
+      $("<div>").addClass("row", "time-block").attr("id", "time-block")
     );
 
     $("#time-block").append(
-        $('<text>')
-            .addClass("hour")
-            .attr("id", "hour")
-            .text(hour + ":00"));
+      $("<text>")
+        .addClass("hour")
+        .attr("id", "hour")
+        .text(hour + ":00")
+    );
+
+    $("#time-block").append($("<textarea>").attr("id", "user-input").val(""));
 
     $("#time-block").append(
-        $('<textarea>'));
-
-    $("#time-block").append(
-        $('<button>')
-            .addClass("saveBtn")
-            .attr("id", "saveBtn")
-            .text("save"));
+      $("<button>").addClass("saveBtn").attr("id", "saveBtn").text("save")
+    );
+  
+    }
     
-}
-// Current hour is red 
-// When user types in plans and presses save button
-    // Checks for input and if not input error message, if else append text to div
-
-
-//variable and function for current time
-// var date = new Date();
-// var time = date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-// console.log(time);
-
-    // if time is 9:00-9:59.59 current background red .present
-    // if time is greater than time value- current background is green .future
-
+    
+  //saves task/plan user types in when save button is clicked
+  $(document).on("click", "#saveBtn", function () {
+    $(this).each(function() {
+      //value of user input task
+      var task = $("#user-input").val();
+      localStorage.setItem("plans", task);
+})
+})
 })
